a1f7528fe1c8b72ec294135a3e880e13cd9d0ae5
Victor Dibia <victor.dibia@gmail.com>
2021-04-29 23:39:47 -0700

    add v1 for localizer

---
+++ signver/localizer/__init__.py
@@ -0,0 +1,1 @@
+from signver.localizer.localizer import Localizer
--- signver/localizer/localizer.py
+++ signver/localizer/localizer.py
@@ -1,13 +1,19 @@
 import tensorflow as tf
+import time
 
 
 class Localizer():
-    def __init__(self) -> None:
-
+    def __init__(self, detect_threshold=0.5) -> None:
+        self.model_load_time = None
+        self.detect_fn = None
+        self.detect_threshold = detect_threshold
         pass
 
     def load(self, model_path: str) -> None:
-        self.model_fn = tf.saved_model.load(model_path)
+        start_time = time.time()
+        self.detect_fn = tf.saved_model.load(model_path)
+        self.model_load_time = time.time() - start_time
 
     def detect(self, input_tensor):
-        return self.model_fn.detect(input_tensor)
+        detections = self.detect_fn(input_tensor)
+        return detections
